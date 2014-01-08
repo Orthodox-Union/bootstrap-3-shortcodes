cf894d6414d199cd74f4bdbe85e34b60918936c0
shelomo <shelomo@gmail.com>
2014-01-08 12:29:20 +0200

    removed js

--- bootstrap-shortcodes.php
+++ bootstrap-shortcodes.php
@@ -40,7 +40,7 @@ function bootsrap_shortcodes_scripts()  {
   wp_enqueue_script( 'bootsrap-shortcodes-popover', BS_SHORTCODES_URL . 'js/bootstrap-shortcodes-popover.js', array( 'jquery' ), false, true );
 
 }
-add_action( 'wp_enqueue_scripts', 'bootsrap_shortcodes_scripts' ); // Register this fxn and allow Wordpress to call it automatcally in the header
+//add_action( 'wp_enqueue_scripts', 'bootsrap_shortcodes_scripts' ); // Register this fxn and allow Wordpress to call it automatcally in the header
 
 // Begin Shortcodes
 class BoostrapShortcodes {
